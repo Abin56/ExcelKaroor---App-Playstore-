b0bbe616199a49a6a51b16a94d7116f07c4da1fb
Abin56 <abinjohn8089@gmail.com>
2024-06-03 12:14:34 +0530

    2 june first commit --@edwin

--- lib/controllers/attendence_controller/attendence_controller.dart
+++ lib/controllers/attendence_controller/attendence_controller.dart
@@ -97,41 +97,60 @@ class AttendanceController extends GetxController {
     }
   }
 
-  Future<void> sendPushMessage(String token, String body, String title) async {
-    log("messageOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
-    try {
-      final reponse = await http.post(
-        Uri.parse('https://fcm.googleapis.com/fcm/send'),
-        headers: <String, String>{
-          'Content-Type': 'application/json',
-          'Authorization':
-              'key=AAAAT5j1j9A:APA91bEDY97KTVTB5CH_4YTnLZEol4Z5fxF0fmO654V7YJO6dL9TV_PyIfv64-pVDx477rONsIl8d63VjxT793_Tj4zuGg32JTy_wUNQ4OhGNbr0KOS2i4z7JaG-ZtENTBpYnEGh-ZLg'
+Future<void> sendPushMessage(
+    String body, String title, String serverKey,String device_id) async {
+  final Uri url = Uri.parse(
+      'https://fcm.googleapis.com/v1/projects/pushnotification-23-may/messages:send');
+
+  final Map<String, dynamic> message = {
+    'message': {
+      'token': device_id,
+      'notification': {
+        'title': title,
+        'body': body,
+      },
+      'android': {
+        'notification': {
+          'title': 'Breaking News',
+          'body': 'Check out the Top Story.',
+          'click_action': 'TOP_STORY_ACTIVITY'
         },
-        body: jsonEncode(
-          <String, dynamic>{
-            'priority': 'high',
-            'data': <String, dynamic>{
-              'click_action': 'FLUTTER_NOTIFICATION_CLICK',
-              'status': 'done',
-              'body': body,
-              'title': title,
-            },
-            "notification": <String, dynamic>{
-              'title': title,
-              'body': body,
-              'android_channel_id': 'high_importance_channel'
-            },
-            'to': token,
-          },
-        ),
-      );
-      log(reponse.body.toString());
-    } catch (e) {
-      if (kDebugMode) {
-        log("error push Notification");
-      }
+        'data': {'story_id': 'story_12345'}
+      },
+      'apns': {
+        'payload': {
+          'aps': {'category': 'NEW_MESSAGE_CATEGORY'}
+        },
+      },
+      'data': {
+        'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+        'status': 'done',
+        'body': body,
+        'title': title,
+      },
+    },
+  };
+
+  try {
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $serverKey',
+      },
+      body: jsonEncode(message),
+    );
+
+    if (response.statusCode == 200) {
+      print('Notification sent successfully!');
+    } else {
+      print('Failed to send notification: ${response.statusCode}');
+      print('Response body: ${response.body}');
     }
+  } catch (e) {
+    print('Exception caught sending notification: $e');
   }
+}
 
   Future<void> getNotificationTimer() async {
     var vari = await FirebaseFirestore.instance
@@ -154,10 +173,10 @@ class AttendanceController extends GetxController {
               .doc(abStsParentUIDList[i].parentId)
               .get()
               .then((usersDeviceIDvalue) async {
-            await sendPushMessage(
-                usersDeviceIDvalue.data()?['devideID'],
-                'Sir/Madam, your child is absent on for $subject period at ${timeformated.value} on ${dateformated.value},\n സർ/മാഡം, ${dateformated.value} തീയതി ${timeformated.value} ഉണ്ടായിരുന്ന $subject പീരീഡിൽ നിങ്ങളുടെ കുട്ടി ഹാജരായിരുന്നില്ല',
-                'Absent Notification from ${abStsParentUIDList[i].studentName}');
+            // await sendPushMessage(
+            //     usersDeviceIDvalue.data()?['devideID'],
+            //     'Sir/Madam, your child is absent on for $subject period at ${timeformated.value} on ${dateformated.value},\n സർ/മാഡം, ${dateformated.value} തീയതി ${timeformated.value} ഉണ്ടായിരുന്ന $subject പീരീഡിൽ നിങ്ങളുടെ കുട്ടി ഹാജരായിരുന്നില്ല',
+            //     'Absent Notification from ${abStsParentUIDList[i].studentName}');
           });
         }
       });
